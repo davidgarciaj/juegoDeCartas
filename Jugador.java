b0a2f6c88d846ffc04183ee887cd94a1b339f377
unknown <you@example.com>
2016-02-05 12:44:00 +0100

    Clase jugador creada

---
+++ Jugador.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Jugador here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jugador
+{
+    //Atributo que almacena la id del jugador
+    private int id;
+    //Atributo que almacena la coleccion de cartas que tiene el jugador.
+    private ArrayList<Carta> cartas;
+    /**
+     * Constructor for objects of class Jugador
+     */
+    public Jugador(int id)
+    {
+        this.id = id;
+    }
+    
+    /**
+     * Método que devuelve la id del jugador.
+     */
+    public int getId()
+    {
+        return id;
+    }
+    
+    /**
+     * Método que le da una carta al jugador.
+     */
+    public void recibirCarta(Carta unaCarta)
+    {
+        cartas.add(unaCarta);
+    }
+    
+    /**
+     * Método que devuelve las cartas que tiene el jugador en la mano.
+     */
+    public void cartasQueTieneEnLaMano()
+    {
+        for (Carta carta : cartas){
+            System.out.println(carta.getNombre);
+        }
+    }
+}
